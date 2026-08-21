8fd7348baa4741740fc363e7b3667e306bd1be70
DonVicB <donvicb.db@gmail.com>
2026-08-21 14:25:24 +1000

    content: ground every answer explanation in its scenario

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='securityplus701-v11';
+const CACHE='securityplus701-v12';
 const ASSETS=['./','./index.html','./styles.css','./exam-1.js','./exam-2.js','./exam-3.js','./exam-4.js','./exam-5.js','./app-1.js','./app-2.js','./app-3.js','./app-4.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
